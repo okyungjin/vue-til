2ef4a39c9d60482ea3713281b0066af60584aced
okyungjin <okyungjin@gmail.com>
2022-06-12 22:53:21 +0900

    FEAT Add router-link in header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <header>
+      <router-link to="/login">로그인</router-link> |
+      <router-link to="/sign-up">회원가입</router-link>
+    </header>
+
     <router-view />
   </div>
 </template>
